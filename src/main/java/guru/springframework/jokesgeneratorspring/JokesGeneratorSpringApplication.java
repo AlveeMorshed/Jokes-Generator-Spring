372922f08c98aec1e7dc13a46c1aa431f2168aa6
AlveeMorshed <56661935+AlveeMorshed@users.noreply.github.com>
2022-08-14 22:28:36 +0600

    Initializing project. Closes #1

---
+++ src/main/java/guru/springframework/jokesgeneratorspring/JokesGeneratorSpringApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokesgeneratorspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JokesGeneratorSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JokesGeneratorSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/jokesgeneratorspring/JokesGeneratorSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.jokesgeneratorspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokesGeneratorSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
