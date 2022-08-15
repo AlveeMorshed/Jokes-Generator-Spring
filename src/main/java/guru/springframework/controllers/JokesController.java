02b397797660fd269a3acc983e30ae4f5cb7ac61
AlveeMorshed <56661935+AlveeMorshed@users.noreply.github.com>
2022-08-15 10:20:41 +0600

    Created View template and moved the Application class outside a problematic directory. Closes #5

--- src/main/java/guru/springframework/controllers/JokesController.java
+++ src/main/java/guru/springframework/controllers/JokesController.java
@@ -4,16 +4,19 @@ import guru.springframework.services.JokeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+
 public class JokesController {
+
     private final JokeService jokeService;
 
     public JokesController(JokeService jokeService) {
         this.jokeService = jokeService;
     }
 
-    @RequestMapping({"/",""} )
+    @RequestMapping({"/",""})
     public String showJoke(Model model){
         model.addAttribute("joke", jokeService.getRandomJoke());
         return "index"; //html page name as String
--- src/main/java/guru/springframework/services/JokeService.java
+++ src/main/java/guru/springframework/services/JokeService.java
@@ -1,5 +1,5 @@
 package guru.springframework.services;
 
 public interface JokeService {
-    String getRandomJoke();
+    public String getRandomJoke();
 }
